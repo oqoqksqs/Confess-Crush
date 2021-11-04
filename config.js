2a35df3508f64e8b6e65288780d6db9de99ed8b6
oqoqksqs <93696153+oqoqksqs@users.noreply.github.com>
2021-11-04 11:22:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Hello",
+    introTitle: 'Diễm Hương cute',
+    introDesc: `Chào Diễm Hương,welcome chị đến với trang web của em làm cho chị
+  
+    
+    ,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Chị có đang chăm chỉ học không 🥰',
+    desc: 'Hay là lại tóp tóp ',
+    btnYes: 'Học <33',
+    btnNo: 'Tóp tóp :3',
+    question: 'Em chỉ có món quà nhỏ  <3',
+    btnReply: 'Gửi cho chị <3',
+    reply: 'P/s:Đây là phiên bản mini của quà sinh nhật dh <33333333',
+    mess: 'Dh có thích ko',
+    messDesc: 'Có hay Không ạ',
+    btnAccept: 'Chắc ko ổn r',
+    messLink: 'https://facebook.com/cute/Confess-DiemHuong' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
